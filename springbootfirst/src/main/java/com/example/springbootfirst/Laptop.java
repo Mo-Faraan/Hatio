36202a5430973c4da52d06e63251b145d26e1ee8
Farhan <mdfarhantm29@gmail.com>
2025-04-09 21:23:54 +0530

    container component autowired

---
+++ springbootfirst/src/main/java/com/example/springbootfirst/Laptop.java
@@ -0,0 +1,10 @@
+package com.example.springbootfirst;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop {
+    public void code(){
+        System.out.println("Compiling...");
+    }
+}
--- springbootfirst/src/main/java/com/example/springbootfirst/SpringbootfirstApplication.java
+++ springbootfirst/src/main/java/com/example/springbootfirst/SpringbootfirstApplication.java
@@ -2,13 +2,27 @@ package com.example.springbootfirst;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringbootfirstApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootfirstApplication.class, args);
-		System.out.println("Hello world");
+		// SpringApplication.run() starts the Spring application and returns an ApplicationContext,
+		// which is the container that manages all the beans (objects) in the application
+
+		ApplicationContext context = SpringApplication.run(SpringbootfirstApplication.class, args);
+
+		/*
+		 * We are not using 'new Alien()' here.
+		 * Instead, we're asking Spring's IoC container to give us an instance (a bean) of the Alien class.
+		 * This is the core concept of Dependency Injection: let the framework manage the dependencies.
+		 */
+
+		Alien obj;
+		obj = context.getBean(Alien.class); // Get the Alien bean from the Spring container
+		obj.code();
+
 	}
 
 }
